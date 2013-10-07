1f448f9e87faf0e18ca9f5c83c3079dfef8ca65f
tef <tef@twentygototen.org>
2013-10-07 16:14:04 +0100

    Use Cookie to persist accessibility #106

--- assets/accessibility.js
+++ assets/accessibility.js
@@ -3,11 +3,14 @@
 (function () {
   'use strict';
 
+  document.getElementById('accessibility').classList[document.cookie.indexOf('invert=yep') > -1 ? 'add' : 'remove']('inverted');
+
   if (document.addEventListener && document.documentElement.classList) {
     document.addEventListener('click', function (event) {
       var classes = event.target.classList;
 
       if (classes.contains('color-change')) {
+        document.cookie = classes.contains('wob') ? 'invert=yep' : 'invert=nope';
         document.getElementById('accessibility').classList[classes.contains('wob') ? 'add' : 'remove']('inverted');
         event.preventDefault();
       }
